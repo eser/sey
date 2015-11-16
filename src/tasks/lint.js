1989bdf9cbf5666afbdd2802be215e90497fbcd8
Eser Ozvataf <eser@ozvataf.com>
2015-11-16 04:06:29 +0200

    * linter now works.

--- src/tasks/lint.js
+++ src/tasks/lint.js
@@ -1,19 +1,38 @@
 var lint = function (context) {
     var self = this,
-        eslint = null;
+        eslint = null,
+        config = null;
 
-    // self.processBundle = function (files) {
-    //     if (eslint === null) {
-    //         eslint = require('eslint');
-    //     }
+    self.processBundle = function (files) {
+        if (eslint === null) {
+            eslint = require('eslint');
 
-    //     var options = {};
+            if (context.bundleConfig.eslintConfig !== undefined && context.bundleConfig.eslintConfig !== null) {
+                config = context.bundleConfig.eslintConfig;
+            } else {
+                config = {};
+            }
+        }
 
-    //     var linter = new eslint.CLIEngine(options);
-    //     var report = linter.executeOnFiles(['./main.js']);
+        var linter = new eslint.CLIEngine(config);
 
-    //     return files;
-    // };
+        for (var file in files) {
+            var report = linter.executeOnText(files[file].content, files[file].file);
+
+            for (var i = 0, length = report.results.length; i < length; i++) {
+                var result = report.results[i];
+
+                if (result.errorCount === 0 && result.warningCount === 0) {
+                    continue;
+                }
+
+                console.log(result);
+                process.exit(0);
+            }
+        }
+
+        return files;
+    };
 };
 
 module.exports = lint;
--- src/tasks/transpile.js
+++ src/tasks/transpile.js
@@ -1,18 +1,28 @@
+var deepmerge = require('deepmerge');
+
 var transpile = function (context) {
     var self = this,
-        babel = null;
+        babel = null,
+        config = null;
 
     self.processBundle = function (files) {
         if (babel === null) {
             babel = require('babel-core');
+
+            if (context.bundleConfig.babelConfig !== undefined && context.bundleConfig.babelConfig !== null) {
+                config = context.bundleConfig.babelConfig;
+            } else {
+                config = {};
+            }
         }
 
-        for (var file in files) {
-            var options = {
-                filename: files[file].file,
+        var options = deepmerge(config, {
                 ast: false,
                 presets: ['es2015']
-            };
+            });
+
+        for (var file in files) {
+            options.filename = files[file].file;
 
             files[file].content = babel.transform(files[file].content, options).code;
         }
