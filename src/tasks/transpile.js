2e0dbbbffe54f742da43c9bfe80813d52cca2bf4
Eser Ozvataf <eser@ozvataf.com>
2015-11-23 19:55:55 +0200

    * transpile debug.

--- src/tasks/transpile.js
+++ src/tasks/transpile.js
@@ -6,33 +6,41 @@ var transpile = function () {
         config = null;
 
     self.processBundle = function (bundle, files) {
-        if (babel === null) {
-            babel = require('babel-core');
-
-            if (bundle.config.babelConfig !== undefined && bundle.config.babelConfig !== null) {
-                config = bundle.config.babelConfig;
-            } else {
-                config = {};
-            }
-        }
-
-        var options = deepmerge(config, {
-                ast: false,
-                presets: ['es2015']
-            });
+        var options;
 
         for (var fileKey in files) {
             var file = files[fileKey],
                 token = file.addTask('preprocess');
-
+console.log(token);
             if (token.cached) {
                 continue;
             }
 
+            // load on demand
+            if (babel === null) {
+                babel = require('babel-core');
+    
+                if (bundle.config.babelConfig !== undefined && bundle.config.babelConfig !== null) {
+                    config = bundle.config.babelConfig;
+                } else {
+                    config = {};
+                }
+
+                options = deepmerge(config, {
+                    ast: false,
+                    presets: ['es2015'],
+    
+                    ignore: /(bower_components)|(node_modules)/,
+                    only: null
+                });
+            }
+
+var now = Date.now();
             options.filename = file.relativeFile;
 
             var content = file.getPreviousContent(),
                 result = babel.transform(content, options);
+console.log('transform', Date.now() - now);
 
             file.updateContent(result.code);
         }
